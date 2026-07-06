79cb98e66e45425223aee9d6540cc3ab27746632
ipa-big <big@ipa.fhg.de>
2026-07-06 16:29:13 +0000

    fix: escape password hash in useradd command

--- build-raspios-lite-containerdisk.sh
+++ build-raspios-lite-containerdisk.sh
@@ -214,7 +214,7 @@ apt-get install -qq -y --no-install-recommends linux-image-arm64 grub-efi-arm64 
 systemctl enable ssh
 
 # Create pi user with password 'raspberry' and enable password authentication
-useradd -m -s /bin/bash -p '$6$92aWRGRRk3LXQCt7$S.50viVL85eKoqZkEIS4/NrA2f/RxJe1jeWSN7dvZ9nYaizJOVHGb3Dk4Wkfqajm722SWULWxG81dugPxNpVx/' pi
+useradd -m -s /bin/bash -p '\$6\$92aWRGRRk3LXQCt7\$S.50viVL85eKoqZkEIS4/NrA2f/RxJe1jeWSN7dvZ9nYaizJOVHGb3Dk4Wkfqajm722SWULWxG81dugPxNpVx/' pi
 sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
